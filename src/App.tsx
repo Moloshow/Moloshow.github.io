c0052490f48cc295064939f25fd05f8c5c4b8279
a_aries <a.aries@technologyandstrategy.com>
2026-06-14 11:09:41 +0200

    remove redundant Experience section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Hero } from './components/Hero'
 import { About } from './components/About'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
-import { Experience } from './components/Experience'
 import { Contact } from './components/Contact'
 import { Footer } from './components/Footer'
 import { ResumePage } from './components/ResumePage'
@@ -32,7 +31,6 @@ function App() {
             <About />
             <Projects />
             <Skills />
-            <Experience />
             <Contact />
           </main>
         )}
